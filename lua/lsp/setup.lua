c22a095c0fcbcf499cbb2900b93784904426f3d2
Dephilia <me@dephilia.moe>
2022-10-16 22:32:12 +0800

    feat: Update cmp cap
    Signed-off-by: Dephilia <me@dephilia.moe>

--- lua/lsp/setup.lua
+++ lua/lsp/setup.lua
@@ -109,7 +109,7 @@ require("nvim-lsp-installer").setup({
 })
 
 -- Setup lspconfig.
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 local navic = require("nvim-navic")
 
 -- Rounded hover
